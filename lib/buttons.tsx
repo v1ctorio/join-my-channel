9802f4d068025bbb8ac13526d77e42b95e26717a
Vic <vicxd@pm.me>
2025-12-23 21:48:55 +0100

    types, events and buttons jsx base

---
+++ lib/buttons.tsx
@@ -0,0 +1,7 @@
+/** @jsx jsx */
+/** @jsxImportSource hono/jsx */
+
+import type { FC } from 'hono/jsx'
+const slackButton: FC = (props) => {
+    return (<button>Log in with Slack</button>)
+}
--- lib/slack.ts
+++ lib/slack.ts
@@ -11,7 +11,9 @@ slack.use(async (c, next) => {
   const timestamp = Number(c.req.header('X-Slack-Request-Timestamp')) || 0 
   const slackSignature = c.req.header("x-slack-signature") || ""
 
+  console.log("Received slack req, ", reqBody)
   if ((Math.floor(Date.now() / 1000) - timestamp) > 60 * 5){
+    console.log("rejecting for invalid timestamp")
     return c.text("Invalid timestamp")
   }
   const baseStringToSign = 'v0:' + timestamp + ':' + reqBody
@@ -19,6 +21,7 @@ slack.use(async (c, next) => {
   
   
   if (!safeCompare(signature, slackSignature)){
+    console.log("rejecting for invalid signature")
     return c.text("Invalid signature")
   }
   await next()
@@ -26,11 +29,14 @@ slack.use(async (c, next) => {
 
 
 slack.post("/events",async(c) => {
-  const body = await c.req.json()
+  const body: SlackEventRes = await c.req.json()
+  c.status(200)
+  console.log("got genuine event")
   switch (body["type"]){
     case "url_verification":
       return c.text(body["challenge"])
-      break;
+    case "event_callback":
+      return c.text('')
   }
   return c.text("Hello slack, you seem authentic")
 
@@ -45,7 +51,42 @@ function safeCompare(a: string, b:string): boolean {
   const bufA = Buffer.from(a, 'utf-8')
   const bufB = Buffer.from(b, 'utf-8')
 
+  console.log(`comparing ${a} to ${b}`)//TODO REMOVE THIS IN PRODUCTION DO NOT LEAK ENV IN LOGS GNG
+
   if (bufA.length !== bufB.length) return false;
 
   return timingSafeEqual(bufA,bufB)
 }
+
+interface EventCallback {
+  type: 'event_callback'
+  token: string
+  team_id: string
+  api_app_id: string
+  event_id: string
+  event_time: number
+}
+interface UrlVerificationCallback {
+  token: string,
+  challenge: string,
+  type: 'url_verification'
+}
+interface LinkSharedEvent extends EventCallback {
+  event: {
+  type: string
+  channel: string
+  is_bot_user_member: boolean
+  user: string
+  message_ts: string
+  unfurl_id: string
+  thread_ts: string
+  source: string
+  links: {
+    domain:string,
+    url:string
+  }[]
+  user_locale: string
+  }
+}
+
+type SlackEventRes = LinkSharedEvent | UrlVerificationCallback
